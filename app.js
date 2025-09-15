693890b4d90850aafca9f5230469fc5b9373b9f6
池田竜聖 <suikoman@icloud.com>
2025-09-15 19:21:43 +0900

    app.jsをコミット

---
+++ app.js
@@ -0,0 +1,8 @@
+document.querySelector("#button").addEventListener("click", function(){
+    if (Math.random() <= 0.5) {
+        alert("ピカチュウをゲットした！"); 
+    }else{
+        alert("ざんねん！もうすこしでつかまえられたのに！")
+    }    
+        
+    });
